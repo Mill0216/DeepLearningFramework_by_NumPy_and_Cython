ce2aac8b87c149019951370444cd257fce3381de
Yamaura <46815301+Mill0216@users.noreply.github.com>
2019-12-17 00:47:02 +0900

    Update Optimizers.pyx

--- Optimizers.pyx
+++ Optimizers.pyx
@@ -6,6 +6,7 @@ cimport numpy as np
 ctypedef np.float_t nf64
 ctypedef np.ndarray na
 
+
 cdef class SGD:
     cdef double lr
     cdef int wndim, bndim
@@ -13,49 +14,34 @@ cdef class SGD:
         self.lr = lr
         
     cpdef set_shape(self, weights, biases):
-        #cpdef na[nf64, ndim=1] update_weights_1(
-        #    self, na[nf64, ndim=1] weights, na[nf64, ndim=1] gradW):
-        #    return weights - self.lr*gradW
         return 
     
-    cpdef na[nf64, ndim=2] update_weights(
-        self, na[nf64, ndim=2] weights, na[nf64, ndim=2] gradW):
+    cpdef na update_weights(
+            self, na weights, na gradW):
         return weights - self.lr*gradW
     
     cpdef na[nf64, ndim=1] update_biases(
         self, na[nf64, ndim=1] biases, na[nf64, ndim=1] gradB):
         return biases - self.lr*gradB
-    
-def sgd(lr=0.01):
-    return 'SGD_{}'.format(lr)
         
 
 cdef class MomentumSGD:
     cdef double lr, alpha
-    cdef na wmoment, bmoment
+    cdef public na weights, biases
     def __init__(self, double lr, double alpha):
         self.lr = lr
         self.alpha = alpha
         
     cpdef set_shape(self, na weights, na biases):
-        self.wmoment = np.zeros_like(weights)
-        self.bmoment = np.zeros_like(biases)
-        return self.wmoment
+        self.weights = np.zeros((*np.array(weights).shape))
+        self.biases = np.zeros((*np.array(biases).shape))
+        return self.weights
     
-    cpdef na[nf64, ndim=2] update_weights(
-        self, na[nf64, ndim=2] weights, na[nf64, ndim=2] gradW):
-        self.wmoment = self.alpha*self.wmoment - self.lr*gradW
-        return weights + self.wmoment
+    cpdef na update_weights(self, na weights, na gradW):
+        self.weights = self.alpha*self.weights - (1-self.alpha)*self.lr*gradW
+        return weights + self.weights
     
     cpdef na[nf64, ndim=1] update_biases(
         self, na[nf64, ndim=1] biases, na[nf64, ndim=1] gradB):
-        self.bmoment = self.alpha*self.bmoment - self.lr*gradB
-        return biases + self.bmoment
-    
-    cpdef na[nf64, ndim=2] update_biases_2(
-        self, na[nf64, ndim=2] biases, na[nf64, ndim=2] gradB):
-        self.bmoment = self.alpha*self.bmoment - self.lr*gradB
-        return biases + self.bmoment
-    
-def momentum_sgd(lr=0.01, alpha=0.9):
-    return 'MomentumSGD_{},{}'.format(lr, alpha)
+        self.biases = self.alpha*self.biases - (1-self.alpha)*self.lr*gradB
+        return biases + self.biases
